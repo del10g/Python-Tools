875daad75d6d8b9aa4120fcbfa22e84c76a98239
del10g <delledu35@gmail.com>
2023-03-31 22:41:15 +1100

    Added to github

---
+++ main.py
@@ -0,0 +1,7 @@
+from tkinter import Tk
+class LisanTools:
+    def __init__(self):
+        print("This is tools used python to display\nNetworking tools ")
+
+app = Tk()
+app.title('Python Help Tools')
